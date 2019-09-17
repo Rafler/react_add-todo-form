725db5fe431bd597f87cec089852611f601131f6
Rafler <amuronyk@gmail.com>
2019-09-17 19:43:26 +0300

    Updates

---
+++ precache-manifest.8046f27d574a0b1b959ed05dbb0a135a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347344892c81215d82f2f482f181871e",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "1c6d239145cce1bcb445",
+    "url": "/react_add-todo-form/static/css/main.a257d536.chunk.css"
+  },
+  {
+    "revision": "74d3da8647cfab6a1ef3",
+    "url": "/react_add-todo-form/static/js/2.c55bae82.chunk.js"
+  },
+  {
+    "revision": "1c6d239145cce1bcb445",
+    "url": "/react_add-todo-form/static/js/main.304bc4cf.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
